4ac9560943e2d4b49c8922f19affda892352247e
Jan-zou <zjl_717@sina.com>
2016-01-19 23:15:23 +0800

    finished problems 1,2, and 6 in python

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# LeetCode
-Solution LeetCode in Python
+# [LeetCode](https://leetcode.com/problemset/algorithms/) ![Language](https://img.shields.io/badge/language-Python-orange.svg) [![License](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE.md)
+
+There are my solution for questions on LeetCode Online Judge in Python.
+
+## Algorithms
+
+* Array
+* Hash Table
+* Linked List
+* Math
+* Two Pointers
+* String
+* Divide and Conquer
+* Binary Search
+
+
+## Problem List
+ #  | Problem         |  Time           | Space           | Difficulty    | Tag          |
+-----|---------------- | --------------- | --------------- | --------------- | ------------- |
+1| [Two Sum](https://leetcode.com/problems/two-sum/)      | _O(n)_         | _O(n)_          | Medium         | Array, Hash Table  |
+2| [Add Two Nmbers](https://leetcode.com/problems/add-two-numbers/)      | _O(n)_         | _O(1)_          | Medium         | Linked List, Math  |
+3| [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)      | _O()_         | _O()_          | Medium         | Hash Table, Two Pointers, String  |
+6| [zigzag Conversion](https://leetcode.com/problems/zigzag-conversion/)    | _O(n)_    | _O(1)_    | Easy | String
 
---
+++ python/2_add_two_numbers.py
@@ -0,0 +1,51 @@
+# !/usr/bin/env python
+# coding: utf-8
+
+'''
+Description:
+    You are given two linked lists representing two non-negative numbers.
+    The digits are stored in reverse order and each of their nodes contain a single digit.
+    Add the two numbers and return it as a linked list.
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+
+Tags: Linked List, Math
+Time: O(n)
+Space: O(1)
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current, carry = dummy, 0
+
+        while l1 != None or l2 != None:
+            temp = carry
+            if l1 is not None:
+                temp += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                temp += l2.val
+                l2 = l2.next
+            carry, temp = temp / 10, temp % 10    # empty carry, and get real temp
+            current.next = ListNode(temp)
+            current = current.next
+
+        if carry == 1:
+            current.next = ListNode(1)
+
+        return dummy.next
+
+
+if __name__ == '__main__':
+    a, a.next, a.next.next = ListNode(2), ListNode(4), ListNode(3)
+    b, b.next, b.next.next = ListNode(5), ListNode(6), ListNode(4)
+    result = Solution().addTwoNumbers(a, b)
+    print "{0} -> {1} -> {2}".format(result.val, result.next.val, result.next.next.val)
