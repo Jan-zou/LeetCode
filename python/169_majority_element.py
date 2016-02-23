5997b54079860c7d93c353ff22b466d7f84688a4
Jan-zou <zjl_717@sina.com>
2016-02-23 21:41:37 +0800

    finished 169 and 217 with tag of array etc.

---
+++ python/169_majority_element.py
@@ -0,0 +1,33 @@
+'''
+Description:
+    Given an array of size n, find the majority element.
+    The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+    You may assume that the array is non-empty and the majority element always exist in the array.
+
+Tags: Divide and Conquer, Array, Bit Manipulation
+
+成对消除
+
+Time:  O(n)
+Space: O(1)
+'''
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counter, item = 0, 0
+        for i in nums:
+            if counter == 0:
+                item = i
+                counter += 1
+            elif item == i:
+                counter += 1
+            else:
+                counter -= 1
+        return item
+
+if __name__ == '__main__':
+    Solution().majorityElement([1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 6])
---
+++ python/217_contains_duplicate.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Description:
+    Given an array of integers, find if the array contains any duplicates.
+    Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Tags: Array, Hash Table
+'''
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) > len(set(nums))
